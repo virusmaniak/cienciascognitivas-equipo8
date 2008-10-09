e6963f71a9b14718e479f59b0b7222cdb60b10d6
yo@salvadorgzz.com <yo@salvadorgzz.com@bea0f324-9564-11dd-a9c7-afc26c2d9d5c>
2008-10-09 03:06:46 +0000

    Avance en el funcionamiento de la preguntas.

--- trunk/scripts/preguntas.js
+++ trunk/scripts/preguntas.js
@@ -1,19 +1,32 @@
 
 var preguntasHechas = 0;
+var tipoDePregunta = 0; // 1 Para color, 2 para forma
 var aciertos = 0;
 var preguntaColor = "De qué color es la figura número ";
 var preguntaForma = "Qué forma tiene la figura número ";
+var colores = new Array ("red", "blue", "yellow");
 
 function makeDivPregunta(){
+    tipoDePregunta = Math.floor(Math.random()*2 + 1);
     pregunta = "";
-    if ( Math.random() > .5 ){
+    if ( tipoDePregunta == 1 ){
         pregunta = preguntaColor;
     } else {
         pregunta = preguntaForma;
     }
     num = Math.floor( Math.random()*3 + 1 );
     return "<div id=\"pregunta" + (++preguntasHechas) +
-                            "\">¿" + pregunta + num + "?</div>";
+                            "\">¿" + pregunta + num + "?<br>" +
+                            makeDivRespuesta(tipoDePregunta, num) + "</div>";
+}
+
+function makeDivRespuesta(tipoDePregunta, num){
+    respuestas = ""
+    for (i = 1; i<= 3; i++){
+        respuestas += "<div id=\"pregunta" + preguntasHechas + "respuesta" + i + "\" title=\"";
+        respuestas += colores[i]  +"\">" + " </div>";
+    }
+    return respuestas;
 }
 
 function formatDivPregunta(elemento){
@@ -31,16 +44,35 @@ function formatDivPregunta(elemento){
             .css("text-shadow", "2px 2px 3px blue")
             .css("text-align", "center")
             .css("font-weight", "bold")
-            .css("font-size", "60px")
+            .css("font-size", "40px")
             .css("font-family", "Helvetica, Verdana, Arial, sans-serif");
+}
+
+function formatDivRespuestas(){
+    for (i = 1; i <= 3; i++){
+        elemento = $("#pregunta" + preguntasHechas + "respuesta" + i);
+        elemento.css("visibility", "visible")
+                .css("width", "50px")
+                .css("height", "50px")
+                .css("background-color", colores[i-1]+"")
+                .css("position", "absolute")
+                .css("left", (i*80+20)+"px")
+                .css("border-style", "solid")
+                .css("border-width", "1px")
+                .css("top", "100px")
+    }
 }
 
 function mostrarPregunta(){
     div = makeDivPregunta();
     $("body").append(div);
     formatDivPregunta($("#pregunta" + preguntasHechas));
+    formatDivRespuestas();
     $("#pregunta"+preguntasHechas).click( function() {
         $(this).css("visibility", "hidden");
+        $(this).contents().each( function(){
+           $(this).css("visibility", "hidden"); 
+        });
         if (preguntasHechas < preguntasTotales){
             setTimeout( mostrarPregunta, 10000 );
         }
@@ -48,5 +80,5 @@ function mostrarPregunta(){
 }
 
 function startPreguntas(){
-    setTimeout( mostrarPregunta, 10000 );
+    setTimeout( mostrarPregunta, 1000 );
 }
