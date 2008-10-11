026f6725af7acdbd19611cf8b4008a66f2db2c16
yo@salvadorgzz.com <yo@salvadorgzz.com@bea0f324-9564-11dd-a9c7-afc26c2d9d5c>
2008-10-11 19:14:44 +0000

    El servidor ya crea juegos y guarda los datos sobre el juego que se va a jugar para que se pueda avanzar entre pagina y pagina. Además un esqueleto de la pagina inicio.html usada para probar el sistema pero que debe de servir para crecerla en la pagina inicio.html final

--- trunk/userModel.py
+++ trunk/userModel.py
@@ -7,14 +7,20 @@ from google.appengine.ext import db
 # TODO: Validar el formato de la base de datos, el registro de Actividad
 
 class registroDeUsuario(db.Model):
+    #info del usuario:
     usuario = db.UserProperty()
     nombre = db.StringProperty()
     edad = db.StringProperty()
+    #info del ultimo juego:
+    tipoDePreguntas = db.IntegerProperty()
+    datos = db.StringProperty()
+    img1 = db.IntegerProperty()
+    img2 = db.IntegerProperty()
+    img3 = db.IntegerProperty()
+    
 
 class registroDeActividad(db.Model):
     usuario = db.UserProperty(required=True)
-    #fecha
+    fecha = db.DateProperty(required=True)
     aciertos = db.IntegerProperty(required=True, default=0)
     preguntas = db.IntegerProperty(required=True, default=0)
-    
-    
