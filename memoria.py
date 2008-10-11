970689a8d52a3727460a08e689b3e687e01d189f
yo <yo@salvadorgzz.com>
2008-10-11 19:14:44 +0000

    El servidor ya crea juegos y guarda los datos sobre el juego que se va a jugar para que se pueda avanzar entre pagina y pagina. Además un esqueleto de la pagina inicio.html usada para probar el sistema pero que debe de servir para crecerla en la pagina inicio.html final

--- memoria.py
+++ memoria.py
@@ -9,6 +9,7 @@ Copyright (c) 2008 __MyCompanyName__. All rights reserved.
 
 import wsgiref.handlers
 import os
+import random
 
 from google.appengine.api import users
 from google.appengine.ext.webapp import template
@@ -17,25 +18,6 @@ from google.appengine.ext import db
 from userModel import registroDeUsuario
 
 # TODO: Clase que reciba los scores del usuario y los guarde en la base de datos
-
-class Menu(webapp.RequestHandler):
-    def get(self):
-        user = users.get_current_user()
-        if not user:
-            self.redirect('/')
-            return
-        
-        registro = db.GqlQuery("SELECT * FROM registroDeUsuario WHERE usuario = :1", user).get()
-        
-        template_values = {
-            'nombre': registro.nombre,
-            'menu_url': '/menu',
-            'sudoku_url': '/memoria/juegos/sudoku',
-            'memorama_url': '/memoria/juegos/memorama',
-            'crucigrama_url': '/memoria/juegos/crucigrama'
-        }
-        path = os.path.join(os.path.dirname(__file__), 'Paginas/juegos.html')
-        self.response.out.write(template.render(path, template_values))
         
 class Start(webapp.RequestHandler):
     # TODO: Hacer pagina que muestre al usuario algunas figuras y deje escoger un juego
@@ -46,15 +28,30 @@ class Start(webapp.RequestHandler):
             return
 
         registro = db.GqlQuery("SELECT * FROM registroDeUsuario WHERE usuario = :1", user).get()
-
+        
+        random.seed()
+        stringDato = """[ ["Cristobal Colon descubrio America", "Colon"], ["El tec fue fundado por Eugenio Garza Sada", "tec"] ]"""
+        registro.tipoDePregunta = random.randint(1,2)
+        registro.datos = stringDato
+        registro.img1 = random.randint(1,15)
+        registro.img2 = random.randint(1,15)
+        registro.img3 = random.randint(1,15)
+        
+        registro.put()
+        
         template_values = {
             'nombre': registro.nombre,
             'menu_url': '/menu',
             'sudoku_url': '/memoria/juegos/sudoku',
             'memorama_url': '/memoria/juegos/memorama',
-            'crucigrama_url': '/memoria/juegos/crucigrama'
+            'crucigrama_url': '/memoria/juegos/crucigrama',
+            'tipoDePreguntas': registro.tipoDePregunta,
+            'datos': registro.datos,
+            'img1': registro.img1,
+            'img2': registro.img2,
+            'img3': registro.img3
         }
-        path = os.path.join(os.path.dirname(__file__), 'Paginas/juegos.html')
+        path = os.path.join(os.path.dirname(__file__), 'Paginas/inicio.html')
         self.response.out.write(template.render(path, template_values))
 
 class End(webapp.RequestHandler):
@@ -72,7 +69,12 @@ class End(webapp.RequestHandler):
             'menu_url': '/menu',
             'sudoku_url': '/memoria/juegos/sudoku',
             'memorama_url': '/memoria/juegos/memorama',
-            'crucigrama_url': '/memoria/juegos/crucigrama'
+            'crucigrama_url': '/memoria/juegos/crucigrama',
+            'tipoDePreguntas': registro.tipoDePregunta,
+            'datos': registro.datos,
+            'img1': registro.img1,
+            'img2': registro.img2,
+            'img3': registro.img3
         }
         path = os.path.join(os.path.dirname(__file__), 'Paginas/juegos.html')
         self.response.out.write(template.render(path, template_values))
@@ -89,10 +91,15 @@ class Juegos(webapp.RequestHandler):
         template_values = {
             'nombre': registro.nombre,
             'hacerPreguntas': 'true',
-            'return_url': '/games',
+            'return_url': '/memoria',
             'titulo': 'Error',
-            'redirect_url': '/games',
-            'mensaje': 'El juego no pudo ser cargado'
+            'redirect_url': '/memoria',
+            'mensaje': 'El juego no pudo ser cargado',
+            'tipoDePreguntas': registro.tipoDePregunta,
+            'datos': registro.datos,
+            'img1': registro.img1,
+            'img2': registro.img2,
+            'img3': registro.img3
         }
         
         template_url = 'Paginas/mensaje.html'
@@ -109,9 +116,8 @@ class Juegos(webapp.RequestHandler):
 
 def main():
     application = webapp.WSGIApplication([('/memoria/juegos.*', Juegos),
-                                            ('/memoria/start', Start),
                                             ('/memoria/end', End),
-                                            ('/memoria', Menu)], debug=True)
+                                            ('/memoria', Start)], debug=True)
     wsgiref.handlers.CGIHandler().run(application)
 
 if __name__ == '__main__':
